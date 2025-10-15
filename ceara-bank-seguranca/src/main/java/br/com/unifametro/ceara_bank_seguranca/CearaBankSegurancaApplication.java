7b12eb74f1b4c81a79eebdb9b90100c5c63f2a66
Vicente Junior <vicentemagalhaesjunior@gmail.com>
2025-10-15 17:44:41 +0000

    Initial push

---
+++ ceara-bank-seguranca/src/main/java/br/com/unifametro/ceara_bank_seguranca/CearaBankSegurancaApplication.java
@@ -0,0 +1,13 @@
+package br.com.unifametro.ceara_bank_seguranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CearaBankSegurancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CearaBankSegurancaApplication.class, args);
+	}
+
+}
---
+++ ceara-bank-seguranca/src/test/java/br/com/unifametro/ceara_bank_seguranca/CearaBankSegurancaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unifametro.ceara_bank_seguranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CearaBankSegurancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
