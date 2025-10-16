31e5251887b521a39216bed533dacd2b585b461e
Vicente Junior <vicentemagalhaesjunior@gmail.com>
2025-10-16 16:42:05 +0000

    Adicionando

--- ceara-bank-seguranca/src/main/java/br/com/unifametro/cearabank/seguranca/config/OpenApiConfig.java
+++ ceara-bank-seguranca/src/main/java/br/com/unifametro/cearabank/seguranca/config/OpenApiConfig.java
@@ -17,7 +17,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
     info = @Info(
         title = "API Ceará Bank - Microsserviço de Segurança (JWT)", // Título
         version = "1.0.0", // Versão
-        description = "API responsável pela autenticação (Login) e gestão inicial de usuários (Registro) para os demais microsserviços do Ceará Bank. Maxo Vehi!", // Descrição Ceara!
+        description = "API responsável pela autenticação (Login) e gestão inicial de usuários (Registro) para os demais microsserviços do Ceará Bank.", 
         contact = @Contact(
             name = "Unifametro Engenharia de Sofware", // Seu nome e curso
             email = "vicentemagalhaesjunior@cearabank.com" // Seu email
@@ -25,7 +25,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
     ),
     // Define os servidores onde a API pode ser acessada
     servers = {
-        @Server(url = "http://localhost:8082", description = "Servidor Local (Desenvolvimento)"),
+        @Server(url = "http://172.17.0.3:8082", description = "Servidor Local (Desenvolvimento)"),
         @Server(url = "https://api.cearabank.com/security", description = "Servidor de Produção (Exemplo)")
     }
 )
