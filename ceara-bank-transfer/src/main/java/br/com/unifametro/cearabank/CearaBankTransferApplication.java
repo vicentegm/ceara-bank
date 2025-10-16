96821f49174afc3709fb460e8eaedfd2855bf7d0
Vicente Jr <vicentemagalhaesjunior@cearabank.com.br>
2025-10-16 19:33:00 +0000

    Add novo projeto ceara-bank-transfer

---
+++ ceara-bank-transfer/src/main/java/br/com/unifametro/cearabank/CearaBankTransferApplication.java
@@ -0,0 +1,13 @@
+package br.com.unifametro.cearabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CearaBankTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CearaBankTransferApplication.class, args);
+	}
+
+}
